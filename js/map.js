1e7578aad24b1a998abe7d03a30456c848dc168e
St3fGD67 <B3uz@hotmail.com>
2018-09-13 21:27:49 +0200

    test 3

--- js/map.js
+++ js/map.js
@@ -32,3 +32,12 @@ myMap.prototype.initMap = function () {
     this.getJson("../js/restaurants.json");
     this.PlaceService = new google.maps.places.PlacesService(this.map);
 }
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
