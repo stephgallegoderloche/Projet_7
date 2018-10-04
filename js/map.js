58c6b8ac93b2be895d5b5f64856de9bdf258ab5c
St3fGD67 <B3uz@hotmail.com>
2018-10-04 19:16:02 +0200

    mise en place des marqueur pour les resto

--- js/map.js
+++ js/map.js
@@ -2,13 +2,16 @@
  * @constructor
  * @return {object} L' objet map
  */
-/**
+
 class MyMap  {
-    this.map = "" // l'api google n'est pas encore chargé
-    this.PlaceService = ""
-    this.restaurants = restaurant
+    constructor(){
+        
+        this.restaurants    = [] 
+    }
+    
 }
-*/
+maMap = new MyMap()
+
 /**
  * Initailse la map en la centrant par défaut sur Paris
  */
@@ -16,6 +19,7 @@ class MyMap  {
 var map, infoWindow;
 
 function initMap() {
+    
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: -34.397,
@@ -39,16 +43,38 @@ function initMap() {
                 animation: google.maps.Animation.DROP,
                 icon: 'img/my-location.png'
             })
+             
             
             infoWindow.open(map);
             map.setCenter(pos);
-        }, function () {
+        }, 
+        
+         () =>{
             handleLocationError(true, infoWindow, map.getCenter());
         });
     } else {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+    /*() => {*/
+        console.log("ouiiiiiii")
+        maMap.restaurants.forEach((restaurant) => {
+                
+            var pos = {
+            lat: restaurant.position.lat,
+            lng: restaurant.position.lng
+            };
+
+            var titleInfo = `${restaurant.name} ${restaurant.average}`
+            var markerResto = new google.maps.Marker({
+                map: this.map,
+                position: pos,
+                title: titleInfo,
+                animation: google.maps.Animation.DROP,
+                icon: 'img/resto-location.png',
+            })
+        }) 
+    /* }*/
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -59,19 +85,49 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 
-//-----------------------------------------------------------------------------------------
 
+
+//-----------------------------------------------------------------------------------------
+/**
+
+*/
+// Création d'une requête HTTP
+var req = new XMLHttpRequest();
+// Requête HTTP GET synchrone vers le fichier langages.txt publié localement
+req.open("GET", "http://localhost/json/restaurant.json", false);
+// Envoi de la requête
+req.send(null);
+// Affiche la réponse reçue pour la requête
+
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+    var restaurants = new XMLHttpRequest();
+    restaurants.open("GET", url);
+    restaurants.addEventListener("load", function () {
+        if (restaurants.status >= 200 && restaurants.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(restaurants.responseText);
+        } else {
+            console.error(restaurants.status + " " + restaurants.statusText + " " + url);
+        }
+    });
+    restaurants.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    restaurants.send(null);
+}
 //Rechercher le nom de tous les restaurants dans le fichier JSON
 ajaxGet("http://localhost/json/restaurant.json", function (reponse) {
     // Transforme la réponse en tableau d'objets JavaScript
     var restaurants = JSON.parse(reponse);
-    var viewRestaurants = document.getElementById("restautants");
+    var viewRestaurants = document.getElementById("restaurants");
     // Affiche le titre de chaque restaurant
+   
     restaurants.forEach(function (restaurant) {
-        let resto = new Restaurant (restaurant)
-        this.restaurants.push(resto)
+        let resto = new Restaurant(restaurant)
+        maMap.restaurants.push(resto)
         resto.creatRestaurantView()
-       });
+    });
+    
 });
-
-
--- js/restaurant.js
+++ js/restaurant.js
@@ -1,32 +1,45 @@
-
+/**
+ * Nos restaurants
+ * @constructor
+ * @return { object}            L ' objet restaurant
+ * 
+ * @param {object} (position)     La latitude et longitude du restaurant
+ * @param {string} (name)         Le nom du restaurant
+ * @param {string} (adress)       L' adresse du restaurant
+ * @param {number} (average)      La note moyenne du restaurant
+ * @param {object} (photos)       Photo retourné par l' API du restaurant
+ * @param {object} (commentsJson) Indique si l' item est à prendre dans la base JSON ou dans l'API
+ */
 class Restaurant {
     constructor(data) {
-        this.name = data.restaurantName
-        this.adress = data.adress
-        this.position = {
-            lat: data.lat,
-            lng: data.long
-        }
-        this.average = getAverageFromRating()
-        this.element = document.getElementById(data.id)
-        this.marker = new Google.map.marker
+        this.name               = data.restaurantName
+        this.adress             = data.adress
+        this.ratings            = data.ratings
+        this.position           = {
+                                    lat: data.lat,
+                                    lng: data.long
+                                  }
+        this.average            = this.getAverageFromRating()
+        this.element            = document.getElementById(data.id)
     }
     getAverageFromRating() {
         let score = 0
         this.ratings.forEach((ratings) => {
-            score += rating.stars
+            score += ratings.stars
         })
         let average = score / this.ratings.length
+        return average
     }
     getElement() {
         this.element.addEventListener("click", () => {
 
         })
     }
+    
     creatRestaurantView() {
         var viewRestaurant = document.createElement("div");
         viewRestaurant.setAttribute('class', 'restaurant');
-        viewRestaurant.innerHTML = `<div> + this.printRatings < /div>`
+        viewRestaurant.innerHTML = `<div>` + this.printRatings + `< /div>`
            
     }
     printRatings(){
